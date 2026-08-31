b502c88181a0ccca54e2c840000f41b880e6705f
unknown <lab2096@etecjk.local>
2026-08-31 16:59:12 -0300

    primeiro commit

---
+++ calc.php
@@ -0,0 +1,22 @@
+ <?php
+    $operacao= $_POST["operacao"];
+    $number = $_POST["number"];
+    $number2 = $_POST["number2"];
+    $resposta;
+
+    if ($operacao == "+"){
+       $resposta = $number + $number2;
+       echo "A soma é igual $resposta";
+    } elseif($operacao == "-"){
+      $resposta = $number - $number2;
+       echo "A subtração é igual $resposta";
+    }elseif($operacao == "*"){
+      $resposta = $number * $number2;
+       echo "A multiplicação é igual $resposta";
+    }elseif($operacao == "/"){
+      $resposta = $number / $number2;
+       echo "A divisão é igual $resposta";
+    }else{
+       echo "operação invalida";
+    };
+  ?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula do jefão</title>
+</head>
+<body>
+     <form action="calc.php" method="post">
+         <label for="number">Numeros:</label>
+         <input type="number" name="number" >
+
+        <label for="number2">Numeros2:</label>
+         <input type="number" name="number2" >
+
+        <label for="operacao">Numeros2:</label>
+         <input type="text" name="operacao" placeholder="+,-,*,/" >
+
+        <input type="submit" value="Enviar">
+         <input type="reset" value="limpar">
+        
+     </form>
+</body>
+</html>
